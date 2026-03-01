a74c5198c6da0543b92f69352e4c6e33345722f3
PENKOV Miroslav <miroslavpenkov12@gmail.com>
2026-03-01 18:34:11 +0100

    modification structure projet

--- src/mlops_tp/data/loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from mlops_tp.config import RAW_DATA_DIR
-
-def load_train_data():
-    return pd.read_csv(RAW_DATA_DIR / "university_query_train.csv")
-
-def load_test_data():
-    return pd.read_csv(RAW_DATA_DIR / "university_query_test.csv")
